eede674853d946c1811c6675fdfe246a8b9e0487
vivoge <vivoge@163.com>
2016-08-27 23:22:19 +0800

    1

---
+++ express-cokieparser.js
@@ -0,0 +1,7 @@
+var express = require('express');
+var expressSession = require('express-session');
+var app = express();
+
+app.use(session({
+
+}))
--- session_wr.js
+++ session_wr.js
@@ -1,6 +1,7 @@
 var http=require('http');
 var url = require('url');
 var fs = require('fs');
+var querystring =require('querystring');
 var SESSION_KEY = 'SESSION_KEY';
 var sessions = {};
 
@@ -10,25 +11,29 @@ http.createServer(function(req,res){
     var query = urlObj.query;
 
     if(pathname == '/'){
-        var sessionId = req.headers.sessionId;
+        var cookieObj = querystring.parse(req.headers.cookie,'; ');
+        var sessionId = cookieObj[SESSION_KEY];
+
         if(sessionId){
+            var sessionObj = sessions[sessionId];
+            if(sessionObj){
+                sessionObj.balance = sessionObj.balance-10;
+                res.setHeader('Content-Type','text/html;charset=utf-8');
+                res.end('余额被扣'+sessionObj.balance)
+            }else{
+                res.end('guoqi')
+            }
 
         }else{
             var sid = Date.now()+''+Math.random();
             sessions[sid] = {balance:100};
-            res.setHeader(SESSION_KEY,sid);
+            res.setHeader('Content-Type','text/html;charset=utf-8');
+            res.setHeader('Set-Cookie',SESSION_KEY+'='+sid);
+            res.end('新卡获赠'+sessions[sid].balance);
         }
 
 
     }
-
-
-
-    else if(pathname == '/read'){
-
-    }
-
-
 
 
 
