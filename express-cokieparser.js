96d5586e7eda474887b44d7c4f51d46cfebfa3b6
vivoge <vivoge@163.com>
2016-08-28 00:15:23 +0800

    1

--- express-cokieparser.js
+++ express-cokieparser.js
@@ -1,7 +1,27 @@
 var express = require('express');
-var expressSession = require('express-session');
+var expresssession = require('express-session');
 var app = express();
 
-app.use(session({
+app.use(expresssession({
+    resave:true,
+    saveUninitialized:true,
+    secret:'zf'  //秘钥
+}));//req.session
 
-}))
+app.get('/',function(req,res){
+     var islogin = req.session.islogin;
+    if(islogin){
+        res.send('old friend');
+    }else{
+        req.session.islogin = 'true';
+        res.send('new friend');
+    }
+});
+
+
+
+
+
+
+
+app.listen(7070);
